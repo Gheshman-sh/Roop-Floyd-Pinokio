38bb563550ec94162e25c76f520a4db60be6bea8
Gheshman-sh <geshman.sh54@gmail.com>
2025-10-04 14:17:37 -0700

    Update pinokio.js

--- pinokio.js
+++ pinokio.js
@@ -1,4 +1,4 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
   version: "1.0",
@@ -7,14 +7,14 @@ module.exports = {
   icon: "icon.png",
 
   menu: async (kernel, info) => {
-    let installed = info.exists("app/env")
+    let installed = info.exists("app/env");
     let running = {
       install: info.running("install.js"),
       start: info.running("start.js"),
       update: info.running("update.js"),
       reset: info.running("reset.js"),
       link: info.running("link.js")
-    }
+    };
 
     if (running.install) {
       return [{
@@ -22,43 +22,43 @@ module.exports = {
         icon: "fa-solid fa-plug",
         text: "Installing",
         href: "install.js",
-      }]
+      }];
     } else if (installed) {
-        if (running.start) {
-            const localURL = "http://127.0.0.1:7860";
-            return [{
-                default: true,
-                icon: "fa-solid fa-rocket",
-                text: "Open Web UI",
-                href: localURL,
-            }, {
-                icon: 'fa-solid fa-terminal',
-                text: "Terminal",
-                href: "start.js",
-            }];
-        }
+      if (running.start) {
+        const localURL = "http://127.0.0.1:7860";
+        return [{
+          default: true,
+          icon: "fa-solid fa-rocket",
+          text: "Open Web UI",
+          href: localURL,
+        }, {
+          icon: 'fa-solid fa-terminal',
+          text: "Terminal",
+          href: "start.js",
+        }];
       } else if (running.update) {
         return [{
           default: true,
           icon: 'fa-solid fa-circle-up',
           text: "Updating",
           href: "update.js",
-        }]
+        }];
       } else if (running.reset) {
         return [{
           default: true,
           icon: 'fa-solid fa-rotate-left',
           text: "Resetting",
           href: "reset.js",
-        }]
+        }];
       } else if (running.link) {
         return [{
           default: true,
           icon: 'fa-solid fa-link',
           text: "Deduplicating",
           href: "link.js",
-        }]
+        }];
       } else {
+        // Default menu when installed but nothing running
         return [{
           default: true,
           icon: "fa-solid fa-power-off",
@@ -81,16 +81,16 @@ module.exports = {
           text: "<div><strong>Reset</strong><div>Revert to pre-install state</div></div>",
           href: "reset.js",
           confirm: "Are you sure you wish to reset the app?"
-        }]
+        }];
       }
     } else {
+      // App not installed
       return [{
         default: true,
         icon: "fa-solid fa-plug",
         text: "Install",
         href: "install.js",
-      }]
+      }];
     }
   }
-}
-
+};
